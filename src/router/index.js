2fb24800c4ea64ac1af5eb1a9f179f918fe19bae
Amelia Kartika Wijaya <ameliakw@student.ub.ac.id>
2023-05-30 23:35:37 +0700

    add faculty llo route

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FacultyView from "../views/FacultyView.vue";
 import FacutyView from "../views/FacultyView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/faculty",
-      name: "faculty",
-			component: FacutyView
-    },
-    {
       path: '/dashboard',
       name: 'dashboard',
-    }
+      component: DashboardView
+    },
+    {
+      path: "/faculty",
+      name: "faculty",
+			component: FacultyView
+    },
+    {
+      path: "/create-faculty",
+      name: "faculty",
+			component: CreateFacultyView
+    },
+    {
+      path: "/read-faculty",
+      name: "faculty",
+			component: ReadFacultyView
+    },
+    {
+      path: "/update-faculty",
+      name: "faculty",
+			component: UpdateFacultyView
+    },
+    {
+      path: "/deletefaculty",
+      name: "faculty",
+			component: DeleteFacultyView
+    },
+    {
+      path: "/faculty",
+      name: "faculty",
+			component: FacultyView
+    },
+    {
+      path: "/create-llo",
+      name: "llo",
+			component: CreateLLOView
+    },
+    {
+      path: "/read-llo",
+      name: "llo",
+			component: ReadLLOView
+    },
+    {
+      path: "/update-llo",
+      name: "llo",
+			component: UpdateLLOView
+    },
+    {
+      path: "/delete-llo",
+      name: "llo",
+			component: DeleteLLOView
+    },
   ]
 })
 
