4b131383b55174937863123af0bd0c65318e16b8
Amelia Kartika Wijaya <ameliakw@student.ub.ac.id>
2023-05-31 00:00:20 +0700

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import FacultyView from "../views/FacultyView.vue";
-import CreateFacultyView from "../views/CreateFacultyView.vue";
-import ReadFacultyView from "../views/ReadFacultyView.vue";
-import UpdateFacultyView from "../views/UpdateFacultyView.vue";
-import DeleteFacultyView from "../views/DeleteFacultyView.vue";
+// import CreateFacultyView from "../views/CreateFacultyView.vue";
+// import ReadFacultyView from "../views/ReadFacultyView.vue";
+// import UpdateFacultyView from "../views/UpdateFacultyView.vue";
+// import DeleteFacultyView from "../views/DeleteFacultyView.vue";
 import LLOView from "../views/LLOView.vue";
-import CreateLLOView from "../views/CreateLLOView.vue";
-import ReadLLOView from "../views/ReadLLOView.vue";
-import UpdateLLOView from "../views/UpdateLLOyView.vue";
-import DeleteLLOView from "../views/DeleteLLOView.vue";
+// import CreateLLOView from "../views/CreateLLOView.vue";
+// import ReadLLOView from "../views/ReadLLOView.vue";
+// import UpdateLLOView from "../views/UpdateLLOyView.vue";
+// import DeleteLLOView from "../views/DeleteLLOView.vue";
 import DetailAssignmentView from "../views/DetailAssignmentView.vue";
 
 
